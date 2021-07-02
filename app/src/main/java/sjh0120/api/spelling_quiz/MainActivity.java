6796d0d60f94043c63086a2ac10eb432d323dca6
sjh0120 <66623619+sjh0120@users.noreply.github.com>
2021-07-02 09:51:56 +0900

    base
    app base

---
+++ app/src/main/java/sjh0120/api/spelling_quiz/MainActivity.java
@@ -0,0 +1,14 @@
+package sjh0120.api.spelling_quiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
